9c3777012ff64a62962f10cca825888de033653d
Masaya <masaya.nishimura0927@gmail.com>
2025-12-24 17:38:50 +0900

    Remove project configuration and assets for kitchen_log_next

--- app/lib/string-sort.ts
+++
@@ -1,6 +0,0 @@
-import { ShoppingList } from '@/app/lib/definitions';
-
-export function StringSort(ShoppingList: ShoppingList[]) {
-  const sortedList = ShoppingList.sort((a, b) => a.name.localeCompare(b.name));
-  return sortedList;
-}
--- app/lib/zenkaku-hankaku.ts
+++
@@ -1,7 +0,0 @@
-export function ZenkakuToHankaku(str: string) {
-  return str
-    .replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) =>
-      String.fromCharCode(s.charCodeAt(0) - 0xfee0),
-    )
-    .replace(/　/g, ' ');
-}
