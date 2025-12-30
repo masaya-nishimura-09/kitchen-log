964b8572f8dd85238c6588370407b5d38e2d54c0
Masaya <masaya.nishimura0927@gmail.com>
2025-12-30 13:32:16 +0900

    feat: implement shopping list table with selection and filtering functionality

--- src/components/containers/shopping-list/shopping-list-items.tsx
+++ src/components/containers/shopping-list/shopping-list-items.tsx
@@ -1,21 +1,6 @@
 "use client"
 
-import type {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-} from "@tanstack/react-table"
-import {
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table"
-import { MoreHorizontal } from "lucide-react"
-import * as React from "react"
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -24,155 +9,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useShoppingListTable } from "@/hooks/use-shopping-list-table"
 import type { ShoppingListItem } from "@/types/shopping-list/shopping-list-item"
-
-const data: ShoppingListItem[] = [
-  {
-    id: 1,
-    userId: "1",
-    name: "egg",
-    amount: "316",
-    unit: "個",
-    status: false,
-    updatedAt: "",
-    createdAt: "",
-  },
-  {
-    id: 2,
-    userId: "1",
-    name: "apple",
-    amount: "316",
-    unit: "個",
-    status: false,
-    updatedAt: "",
-    createdAt: "",
-  },
-  {
-    id: 3,
-    userId: "1",
-    name: "fish",
-    amount: "316",
-    unit: "個",
-    status: false,
-    updatedAt: "",
-    createdAt: "",
-  },
-]
-
-export const columns: ColumnDef<ShoppingListItem>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "name",
-    header: "名前",
-    cell: ({ row }) => <div>{row.getValue("name")}</div>,
-  },
-  {
-    accessorKey: "amount",
-    header: "量",
-    cell: ({ row }) => <div>{row.getValue("amount")}</div>,
-  },
-  {
-    accessorKey: "unit",
-    header: "単位",
-    cell: ({ row }) => <div>{row.getValue("unit")}</div>,
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const item = row.original
-      console.log(item)
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem>Copy payment ID</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
-  },
-]
+import ShoppingListTable from "./shopping-list-table"
 
 export default function ShoppingListItems({
   shoppingList,
 }: {
   shoppingList: ShoppingListItem[]
 }) {
-  const [sorting, setSorting] = React.useState<SortingState>([])
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    [],
+  const [done, _setDone] = useState<ShoppingListItem[]>(
+    shoppingList.filter((i) => i.status === true),
   )
-  const [columnVisibility, setColumnVisibility] =
-    React.useState<VisibilityState>({})
-  const [rowSelection, setRowSelection] = React.useState({})
+  const [undone, _setUndone] = useState<ShoppingListItem[]>(
+    shoppingList.filter((i) => i.status === false),
+  )
 
-  const table = useReactTable({
-    data,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
-  })
+  const doneTable = useShoppingListTable(done)
+  const undoneTable = useShoppingListTable(undone)
 
   return (
     <Card className="size-full">
@@ -183,87 +38,18 @@ export default function ShoppingListItems({
         </CardAction>
       </CardHeader>
       <CardContent>
-        <div className="w-full">
-          <div className="flex items-center py-4">Header</div>
-          <div className="overflow-hidden rounded-md border">
-            <Table>
-              <TableHeader>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <TableHead key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext(),
-                              )}
-                        </TableHead>
-                      )
-                    })}
-                  </TableRow>
-                ))}
-              </TableHeader>
-              <TableBody>
-                {table.getRowModel().rows?.length ? (
-                  table.getRowModel().rows.map((row) => (
-                    <TableRow
-                      key={row.id}
-                      data-state={row.getIsSelected() && "selected"}
-                    >
-                      {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext(),
-                          )}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell
-                      colSpan={columns.length}
-                      className="h-24 text-center"
-                    >
-                      No results.
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </div>
-          <div className="flex items-center justify-end space-x-2 py-4">
-            <div className="text-muted-foreground flex-1 text-sm">
-              {table.getFilteredSelectedRowModel().rows.length > 0 && (
-                <Button variant="outline">
-                  {table.getFilteredSelectedRowModel().rows.length}
-                  個のステータスを変更
-                </Button>
-              )}
-            </div>
-            <div className="space-x-2">
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-              >
-                前へ
-              </Button>
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-              >
-                次へ
-              </Button>
-            </div>
-          </div>
-        </div>
+        <Tabs defaultValue="undone" className="w-full">
+          <TabsList>
+            <TabsTrigger value="undone">未購入</TabsTrigger>
+            <TabsTrigger value="done">購入済み</TabsTrigger>
+          </TabsList>
+          <TabsContent value="undone">
+            <ShoppingListTable table={undoneTable.table} />
+          </TabsContent>
+          <TabsContent value="done">
+            <ShoppingListTable table={doneTable.table} />
+          </TabsContent>
+        </Tabs>
       </CardContent>
     </Card>
   )
