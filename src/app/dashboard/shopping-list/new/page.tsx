ec38102be3ce383b4473b453ce8900b3b41253ec
Masaya <masaya.nishimura0927@gmail.com>
2025-12-30 11:22:48 +0900

    remove unused shopping list page

--- src/app/dashboard/shopping-list/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import RecipeForm from "@/components/containers/recipe/form"
-
-export default async function Page() {
-  return (
-    <div className="size-full">
-      <RecipeForm recipe={null} mode="new" />
-    </div>
-  )
-}
