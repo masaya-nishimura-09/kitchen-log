15aa77b0bba1a30ae4ad3cd286a9c06e136a46c7
Masaya <masaya.nishimura0927@gmail.com>
2026-01-01 21:30:45 +0900

    refactor set meal page layout to use card components for improved structure and styling

--- src/app/dashboard/set-meal/page.tsx
+++ src/app/dashboard/set-meal/page.tsx
@@ -5,6 +5,13 @@ import { fetchSetMeals } from "@/actions/set-meal/fetch"
 import NoSetMeals from "@/components/containers/set-meal/no-set-meals"
 import SetMealCard from "@/components/containers/set-meal/set-meal-card"
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import type { SearchParams } from "@/types/recipe/search-params"
 import type { SetMeal } from "@/types/set-meal/set-meal"
@@ -28,30 +35,35 @@ export default async function Page(props: {
 
 function SetMealsPage({ setMeals }: { setMeals: SetMeal[] }) {
   return (
-    <div className="size-full flex flex-col gap-2">
-      <div className="flex w-full justify-end items-center gap-2">
-        <form method="GET" className="relative max-w-lg">
-          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            type="search"
-            name="title"
-            placeholder="検索..."
-            className="pl-8 w-full"
-          />
-        </form>
-        <Button type="button">
-          <Link href="/dashboard/set-meal/new">新規追加</Link>
-        </Button>
-      </div>
-      {setMeals.length > 0 ? (
-        <div className="size-full grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4">
-          {setMeals.map((setMeal) => (
-            <SetMealCard key={setMeal.id} setMeal={setMeal} />
-          ))}
-        </div>
-      ) : (
-        <NoSetMeals />
-      )}
-    </div>
+    <Card className="size-full">
+      <CardHeader>
+        <CardTitle>献立</CardTitle>
+        <CardAction className="flex w-full justify-end items-center gap-2">
+          <form method="GET" className="relative max-w-lg">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              name="title"
+              placeholder="検索..."
+              className="pl-8 w-full"
+            />
+          </form>
+          <Button type="button">
+            <Link href="/dashboard/set-meal/new">新規追加</Link>
+          </Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        {setMeals.length > 0 ? (
+          <div className="size-full grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-4">
+            {setMeals.map((setMeal) => (
+              <SetMealCard key={setMeal.id} setMeal={setMeal} />
+            ))}
+          </div>
+        ) : (
+          <NoSetMeals />
+        )}
+      </CardContent>
+    </Card>
   )
 }
