fa61aadbededc5a47a822840fdf2162b5adcb0bd
Masaya <masaya.nishimura0927@gmail.com>
2026-01-02 21:51:23 +0900

    refactor: remove error handling try-catch

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -10,34 +10,29 @@ import LatestShoppingList from "@/components/containers/shopping-list/latest-sho
 import { createClient } from "@/lib/supabase/server"
 
 export default async function Page() {
-  try {
-    const supabase = createClient(cookies())
-    const {
-      data: { user },
-      error,
-    } = await supabase.auth.getUser()
-    if (error || !user?.user_metadata.name) {
-      notFound()
-    }
-
-    const latestRecipes = await fetchLatestRecipes(4)
-    const latestSetMeals = await fetchLatestSetMeals(4)
-    const latestShoppingList = await fetchLatestShoppingList(8)
-
-    if (!latestRecipes || !latestSetMeals || !latestShoppingList) {
-      notFound()
-    }
-
-    return (
-      <div className="size-full flex flex-col gap-4">
-        <Information username={user.user_metadata.name} />
-        <LatestRecipes recipes={latestRecipes} />
-        <LatestSetMeals setMeals={latestSetMeals} />
-        <LatestShoppingList shoppingList={latestShoppingList} />
-      </div>
-    )
-  } catch (error) {
-    console.error("Recipes fetch error:", error)
-    throw error
+  const supabase = createClient(cookies())
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser()
+  if (error || !user?.user_metadata.name) {
+    notFound()
   }
+
+  const latestRecipes = await fetchLatestRecipes(4)
+  const latestSetMeals = await fetchLatestSetMeals(4)
+  const latestShoppingList = await fetchLatestShoppingList(8)
+
+  if (!latestRecipes || !latestSetMeals || !latestShoppingList) {
+    notFound()
+  }
+
+  return (
+    <div className="size-full flex flex-col gap-4">
+      <Information username={user.user_metadata.name} />
+      <LatestRecipes recipes={latestRecipes} />
+      <LatestSetMeals setMeals={latestSetMeals} />
+      <LatestShoppingList shoppingList={latestShoppingList} />
+    </div>
+  )
 }
