b70a0d0618e45bb8c53ae98e9f1a38b744396bf3
Masaya <masaya.nishimura0927@gmail.com>
2026-01-05 21:33:11 +0900

    fix calendar page props

--- src/app/dashboard/calendar/[date]/page.tsx
+++ src/app/dashboard/calendar/[date]/page.tsx
@@ -1,15 +1,13 @@
 import { notFound } from "next/navigation"
 import { fetchEvents } from "@/actions/calendar/fetch"
-import { fetchRecipes } from "@/actions/recipe/fetch"
 import MainCalendar from "@/components/containers/calendar/calendar"
 
 export default async function Page({ params }: { params: { date: string } }) {
   const { date } = await params
-  const recipes = await fetchRecipes(undefined)
   const events = await fetchEvents()
 
-  if (!recipes) {
+  if (!events) {
     notFound()
   }
-  return <MainCalendar recipes={recipes} events={events} defaultDate={date} />
+  return <MainCalendar events={events} defaultDate={date} />
 }
