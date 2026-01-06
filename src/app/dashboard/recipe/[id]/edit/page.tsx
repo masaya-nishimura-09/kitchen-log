e3f46b8b87a802c7d72649cbb6fd46fb6927f96b
Masaya <masaya.nishimura0927@gmail.com>
2026-01-06 16:02:05 +0900

    fix recipe components

--- src/app/dashboard/recipe/[id]/edit/page.tsx
+++ src/app/dashboard/recipe/[id]/edit/page.tsx
@@ -1,9 +1,9 @@
-import { notFound } from "next/navigation"
-import { fetchRecipeInput } from "@/actions/recipe/fetch"
-import RecipeForm from "@/components/containers/recipe/form"
+import {notFound} from "next/navigation"
+import {fetchRecipeInput} from "@/actions/recipe/fetch"
+import RecipeForm from "@/components/containers/recipe/form/recipe-form"
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params
+export default async function Page({params}: {params: {id: string}}) {
+  const {id} = await params
 
   const recipe = await fetchRecipeInput(Number(id))
   if (!recipe) {
